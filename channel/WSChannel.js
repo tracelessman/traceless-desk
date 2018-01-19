a0cc5b3bcad30adf8cc790012bb8a7f00c60ab1e
randomyige <randomyige@gmail.com>
2018-01-19 15:01:19 +0800

    init

---
+++ channel/WSChannel.js
@@ -0,0 +1,161 @@
+/**
+ * Created by renbaogang on 2017/10/28.
+ */
+var WSChannel={
+    seed:0,
+    callbacks:{},
+    generataMsgId : function () {
+        return this.seed++;
+    },
+    newRequestMsg:function (action,data,callback,clientId,targetId) {
+        var id = this.generataMsgId();
+        if(callback)
+            this.callbacks[id] = callback;
+        return  {id:id,action:action,data:data,clientId:clientId,targetId:targetId};//id消息id clientId 身份id
+    },
+    getChannel : function(ip){
+        if(!ip){
+            return;
+        }
+        var url = 'ws://'+ip+':3000/transfer';
+        this.ip = ip;
+        //关掉原来的
+        if(this.ws&&this.ws.ip!=ip){
+            this.ws.close();
+            delete this.ws;
+        }
+        if(!this.ws){
+            console.info("start conn ");
+            this.ws = new WebSocket(url);
+            this.ws.ip=ip;
+
+            this.ws.onmessage = function incoming(message) {
+                var msg = JSON.parse(message.data);
+                var action = msg.action;
+                var isResponse = msg.isResponse;
+                if(isResponse){
+                    if(WSChannel.callbacks[msg.id]){
+                        WSChannel.callbacks[msg.id](msg.data);
+                        delete WSChannel.callbacks[msg.id];
+                    }
+                }else{
+                    WSChannel[action+"Handler"](msg);
+                    WSChannel.ws.send(JSON.stringify({key:msg.key,isResponse:true}));
+                }
+
+            };
+
+            this.ws.onerror = function incoming(event) {
+                // console.log("error: "+event.message);
+            };
+            this.ws.onclose = (event)=>{
+                if(event.target.ip==WSChannel.ip){
+                    delete WSChannel.ws;
+                    setTimeout(()=>{this.getChannel(event.target.ip)},5000);
+                }
+
+            }
+
+
+            //error here
+            this.ws.onopen = function () {
+                console.log("open:"+Store.getCurrentClientId());
+                if(Store.getCurrentClientId()){
+                    WSChannel.login(Store.getName(),Store.getCurrentClientId(),ip,(data,error)=>{
+                        if(error){
+                            alert(error);
+                        }
+                    });
+                }
+            };
+
+        }
+        return this.ws;
+    },
+    //获取公钥私钥
+    generateKey : function(name,ip,callback){
+        var times=0;
+        var ws = this.getChannel(ip);
+        var tmp = function () {
+            if(ws.readyState==ws.OPEN){
+                ws.send(JSON.stringify(WSChannel.newRequestMsg("generateKey",{name:name},callback)));
+            }
+             else if(ws.readyState==ws.CONNECTING&&times<=5){
+                times++;
+                 setTimeout(tmp,2000);
+             }
+            else{
+                callback(null,"无法连接服务器，请确认服务器ip是否正确");
+            }
+        }
+        tmp();
+
+    },
+    //登录后服务端形成公钥和通道形成绑定
+    login:function (name,clientId,ip,callback) {
+        var times=0;
+        var ws = this.getChannel(ip);
+        var tmp = function () {
+            console.info(ws.readyState+":"+ws.OPEN+":"+ws.CONNECTING);
+            if(ws.readyState==ws.OPEN){
+                ws.send(JSON.stringify(WSChannel.newRequestMsg("login",{name:name},callback,clientId)));
+            }
+            else if(ws.readyState==ws.CONNECTING&&times<=3){
+                times++;
+                 setTimeout(tmp,2000);
+            }
+            else{
+                callback(null,"无法连接服务器，请确认服务器ip是否正确");
+            }
+        }
+        tmp();
+
+    },
+    searchFriends:function (searchText,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("searchFriends",{text:searchText},callback)));
+    },
+    applyMakeFriends:function (targetId,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("applyMakeFriends",{name:Store.getName(),publicKey:Store.getPublicKey()},callback,Store.getCurrentClientId(),targetId)));
+    },
+    applyMakeFriendsHandler:function(msg){
+        Store.receiveMKFriends(msg.clientId,msg.data.name,msg.data.publicKey);
+    },
+    acceptMakeFriends:function (targetId,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("acceptMakeFriends",{name:Store.getName(),publicKey:Store.getPublicKey()},callback,Store.getCurrentClientId(),targetId)));
+    },
+    acceptMakeFriendsHandler:function(msg){
+        Store.addFriend(msg.clientId,msg.data.name,msg.data.publicKey);
+    },
+    sendMessage:function (targetId,text,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("sendMessage",{text:text},callback,Store.getCurrentClientId(),targetId)));
+    },
+    sendMessageHandler:function(msg){
+        Store.receiveMessage(msg.clientId,msg.data.text);
+    },
+    sendImage:function (targetId,uri,data,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("sendImage",{data:data},callback,Store.getCurrentClientId(),targetId)));
+    },
+    sendImageHandler:function(msg){
+        Store.receiveImage(msg.clientId,msg.data.data);
+    },
+    addGroup:function (groupId,groupName,members,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("addGroup",{groupId:groupId,groupName:groupName,members:members},callback,Store.getCurrentClientId())));
+    },
+    addGroupHandler:function(msg){
+        Store.addGroup(msg.data.groupId,msg.data.groupName,msg.data.members);
+    },
+    sendGroupMessage:function (groupId,members,text,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("sendGroupMessage",{groupId:groupId,members:members,text:text},callback,Store.getCurrentClientId())));
+    },
+    sendGroupMessageHandler:function(msg){
+        Store.receiveGroupMessage(msg.clientId,msg.data.groupId,msg.data.text);
+    },
+    sendGroupImage:function (groupId,members,uri,data,callback) {
+        this.ws.send(JSON.stringify(WSChannel.newRequestMsg("sendGroupImage",{groupId:groupId,members:members,data:data},callback,Store.getCurrentClientId())));
+    },
+    sendGroupImageHandler:function(msg){
+        Store.receiveGroupImage(msg.clientId,msg.data.groupId,msg.data.data);
+    },
+
+
+};
---
+++ store/Store.js
@@ -0,0 +1,380 @@
+/**
+ * Created by renbaogang on 2017/10/30.
+ */
+
+var Store = {
+    _groupSeed:Date.now(),
+    _listeners:new Map(),
+    on:function (event,fun) {
+        var ary=this._listeners.get(event)
+        if(!ary){
+            ary = [];
+            this._listeners.set(event,ary);
+        }
+        ary.push(fun);
+    },
+    un:function (event,fun) {
+        var ary=this._listeners.get(event);
+        ary.splice(ary.indexOf(fun),1);
+    },
+    _fire:function (event,params) {
+        var ary=this._listeners.get(event)
+        if(ary){
+            ary.forEach(function(o){
+                o(params);
+            });
+        }
+    },
+
+    clientId:null,
+
+    // v:desk
+    _save :function () {
+        localStorage.setItem("data",JSON.stringify(this.data));
+    },
+
+    saveKey:function (name,server,id,publicKey,privateKey,) {
+        if(!this.data){
+            this.data = [];
+        }
+        var update = false;
+        for(var i=0;i<this.data.length;i++){
+            var keyData = this.data[i];
+            if(keyData.id==id){
+                keyData.name = name;
+                keyData.server = server;
+                keyData.publicKey = publicKey;
+                keyData.privateKey = privateKey;
+                update = true;
+                break;
+            }
+        }
+        if(!update)
+            this.data.splice(0,1,{name:name,server:server,id:id,publicKey:publicKey,privateKey:privateKey});
+        this._save();
+    },
+// v:desk
+    fetchAllKeys : function (callback) {
+        if(this.data){
+            callback(this.data);
+        }else{
+            var result = localStorage.getItem("data");
+            if(result){
+                this.data = JSON.parse(result);
+                callback(this.data);
+            }else{
+                callback(null);
+            }
+        }
+    },
+    setCurrentClientId:function (id) {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == id) {
+                this.clientId = id;
+                this.keyData = keyData;
+            }
+        }
+
+    },
+    getCurrentClientId:function () {
+        return this.clientId;
+    },
+    getName:function () {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == this.clientId) {
+                return keyData.name;
+            }
+        }
+    },
+    getPublicKey:function () {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == this.clientId) {
+                return keyData.publicKey;
+            }
+        }
+    },
+    //是否有新的还要申请要处理
+    hasNewReceivedMKFriends:function () {
+        if(!this.keyData.mkfriends){
+            this.keyData.mkfriends={};
+        }
+        return this.keyData.mkfriends.newReceive;
+    },
+    //接收交友请求
+    receiveMKFriends : function (fromId,fromName,publicKey) {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == this.clientId) {
+                if(!keyData.mkfriends){
+                    keyData.mkfriends={};
+                }
+                if(!keyData.mkfriends.receive){
+                    keyData.mkfriends.receive=[];
+                }
+                var all = keyData.mkfriends.receive;
+                var update = false;
+                for(var j=0;j<all.length;j++){
+                    if(all[j].id==fromId){
+                        all[j].name = fromName;
+                        update=true;
+                    }
+                }
+                if(!update){
+                    keyData.mkfriends.receive.push({name:fromName,id:fromId,publicKey:publicKey,state:0});
+                }
+                keyData.mkfriends.newReceive = true;
+                this._save();
+                this._fire("receiveMKFriends",fromName)
+                break;
+            }
+        }
+    },
+    fetchAllReceivedMKFriends:function () {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == this.clientId) {
+                if(!keyData.mkfriends){
+                    keyData.mkfriends={};
+                }
+                if(!keyData.mkfriends.receive){
+                    keyData.mkfriends.receive=[];
+                }
+                if(keyData.mkfriends.newReceive){
+                    keyData.mkfriends.newReceive = false;
+                    this._save();
+                    this._fire("readNewMKFriends")
+                }
+
+                return keyData.mkfriends.receive;
+            }
+        }
+    },
+
+    acceptMKFriends : function (id,callback) {
+        for(var i=0;i<this.data.length;i++) {
+            var keyData = this.data[i];
+            if (keyData.id == this.clientId) {
+                var all = keyData.mkfriends.receive;
+                for(var j=0;j<all.length;j++){
+                    if(all[j].id==id){
+                        all[j].state=1;
+                        this._save();
+                        this.addFriend(all[j].id,all[j].name,all[j].publicKey);
+                        callback();
+                        break;
+                    }
+                }
+            }
+        }
+    },
+
+    addFriend : function (id,name,publicKey) {
+        var all = this.getAllFriends();
+        for(var j=0;j<all.length;j++){
+            if(all[j].id==id){
+                return;
+            }
+        }
+        all.push({id:id,name:name,publicKey:publicKey});
+        this._save();
+        this._fire("addFriend",id);
+    },
+    getAllFriends : function () {
+        if(!this.keyData.friends){
+            this.keyData.friends = [];
+        }
+        return this.keyData.friends;
+    },
+    getAllRecent:function () {
+        if(!this.keyData.recent){
+            this.keyData.recent=[];
+        }
+        return this.keyData.recent;
+    },
+    getFriend:function (id) {
+        var all = this.getAllFriends();
+        for(var j=0;j<all.length;j++){
+            if(all[j].id==id){
+                return all[j];
+            }
+        }
+    },
+    getRecent:function (id,force) {
+        var recents = this.getAllRecent();
+        for(var j=0;j<recents.length;j++){
+            if(recents[j].id==id){
+                return recents[j];
+            }
+        }
+        if(force){
+            var recent = {id:id,records:[],newReceive:false};
+            recents.push(recent)
+            return recent;
+        }
+    },
+    readAllChatRecords : function (id) {
+        var recent = this.getRecent(id,true);
+        recent.newReceive=false;
+        this._save();
+        this._fire("readChatRecords",id)
+        return recent.records;
+    },
+    _getChatRecords:function (id, force, newMsg) {
+        var recent = this.getRecent(id,force);
+        if(recent){
+            if(newMsg){
+                recent.newReceive=true;
+            }
+            return recent.records;
+        }
+    },
+    receiveMessage:function (fromId,text) {
+        var records = this._getChatRecords(fromId,true,true);
+        records.push({id:fromId,text:text});
+        this._save();
+        this._fire("receiveMessage",fromId);
+    },
+    receiveImage:function (fromId,img) {
+        var records = this._getChatRecords(fromId,true,true);
+        records.push({id:fromId,img:img});
+        this._save();
+        this._fire("receiveMessage",fromId);
+    },
+    sendMessage:function (targetId,text) {
+        var records = this._getChatRecords(targetId,true);
+        records.push({text:text});
+        this._save()
+        this._fire("sendMessage",targetId);
+    },
+    sendImage:function (targetId,uri,data) {
+        var records = this._getChatRecords(targetId,true);
+        records.push({img:uri});
+        this._save()
+        this._fire("sendMessage",targetId);
+    },
+    getGroups:function () {
+        var groups = this.keyData.groups;
+        if(!groups){
+            groups = [];
+            this.keyData.groups = groups;
+        }
+        return groups;
+    },
+    addGroup:function (id,name,members) {
+        this.getGroups().push({id:id,name:name,members:members,records:[],newReceive:false});
+        this._save();
+        this._fire("addGroup");
+    },
+    generateGroupId:function () {
+        return this.keyData.id+(this._groupSeed++);
+    },
+    _getGroup:function (id,force) {
+        var groups = this.getGroups();
+        for(var j=0;j<groups.length;j++){
+            if(groups[j].id==id){
+                return groups[j];
+            }
+        }
+        if(force){
+            var g = {id:id,members:[],records:[],newReceive:false};
+            groups.push(g)
+            return g;
+        }
+    },
+    getGroup:function (id) {
+        return this._getGroup(id,false);
+    },
+    readGroupChatRecords:function (id) {
+        var g = this._getGroup(id,true);
+        g.newReceive=false;
+        this._save();
+        this._fire("readGroupChatRecords",id)
+        return g.records;
+    },
+    getMembersBaseInfo:function (gid) {
+        var g = this._getGroup(gid,true);
+        var  ms = [];
+        for(var i=0;i<g.members.length;i++){
+            var m = g.members[i];
+            ms.push({id:m.id,name:m.name});
+        }
+        return ms;
+    },
+    getMember:function (gid,mid) {
+        var g = this._getGroup(gid,true);
+        for(var i=0;i<g.members.length;i++){
+            if(g.members[i].id==mid){
+                return g.members[i];
+            }
+        }
+    },
+    receiveGroupMessage:function (fromId,groupId,text) {
+        var records = this._getGroupChatRecords(groupId,true,true);
+        records.push({id:fromId,text:text});
+        this._save();
+        this._fire("receiveGroupMessage",groupId);
+    },
+    _getGroupChatRecords:function (gid, force, newMsg) {
+        var g = this._getGroup(gid,force);
+        if(g){
+            if(newMsg){
+                g.newReceive=true;
+            }
+            return g.records;
+        }
+    },
+    sendGroupMessage:function (gid,text) {
+        var records = this._getGroupChatRecords(gid,true);
+        records.push({text:text});
+        this._save()
+        this._fire("sendGroupMessage",gid);
+    },
+    receiveGroupImage:function (fromId,groupId,img) {
+        var records = this._getGroupChatRecords(groupId,true,true);
+        records.push({id:fromId,img:img});
+        this._save();
+        this._fire("receiveGroupMessage",groupId);
+    },
+    sendGroupImage:function (gid,uri,data) {
+        var records = this._getGroupChatRecords(gid,true);
+        records.push({img:uri});
+        this._save()
+        this._fire("sendGroupMessage",gid);
+    },
+    reset:function () {
+        this.data = []
+        this.clientId = null;
+        this.keyData = null;
+        this._save();
+    },
+    clear:function () {
+        var recent = this.getAllRecent();
+        recent.forEach(function (r) {
+            r.newReceive=false;
+            r.records=[];
+        });
+        var groups = this.getGroups();
+        groups.forEach(function (r) {
+            r.newReceive=false;
+            r.records=[];
+        });
+        this.clientId = null;
+        this.keyData = null;
+        this._save();
+    }
+    // rejectMKFriends : function (index) {
+    //     for(var i=0;i<this.data.length;i++) {
+    //         var keyData = this.data[i];
+    //         if (keyData.id == this.clientId) {
+    //             keyData.mkfriends.receive[index].state=2;
+    //             this._save();
+    //             break;
+    //         }
+    //     }
+    // }
+};
+
+//获取本地key，无key就引导下载，有则到主界面
